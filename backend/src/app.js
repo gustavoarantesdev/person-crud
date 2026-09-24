8a95911e81acaa02b73b5feeb8c23d95c60fab4e
Gustavo Arantes <gustavoarantesdev@gmail.com>
2026-09-24 15:07:05 -0300

    feat: adiciona arquivo de rotas para o server

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,9 @@
 import express from "express";
+import personRoutes from "./routes/person.routes.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(personRoutes);
 
 export default app;
