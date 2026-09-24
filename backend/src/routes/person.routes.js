677a7b391739ab0e762b6a3f7ca8d48003bb4af6
Gustavo Arantes <gustavoarantesdev@gmail.com>
2026-09-24 15:17:26 -0300

    feat: adiciona rotas base

--- backend/src/routes/person.routes.js
+++ backend/src/routes/person.routes.js
@@ -4,5 +4,9 @@ import * as personController from "../controllers/person.controller.js";
 const router = express.Router();
 
 router.get("/persons", personController.getAll);
+router.get("/persons/:id", personController.getById);
+router.post("/persons", personController.create);
+router.put("/persons", personController.update);
+router.delete("/persons", personController.destroy);
 
 export default router;
