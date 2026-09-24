b19613ac893831a96eb39578bd118d7b4bd06a6e
Gustavo Arantes <gustavoarantesdev@gmail.com>
2026-09-24 16:14:31 -0300

    feat: adiciona lógica para adicionar pessoas

--- backend/src/controllers/person.controller.js
+++ backend/src/controllers/person.controller.js
@@ -11,7 +11,13 @@ export function getById(req, res) {
 }
 
 export function create(req, res) {
-  //
+  const person = personModel.insert(req.body);
+
+  if (person.changes > 0) {
+    return res.status(201).json({ message: "Person created." });
+  }
+
+  return res.status(400).json({ message: "Error registerign the person." });
 }
 
 export function update(req, res) {
